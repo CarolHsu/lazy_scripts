9960a048be30e2d99daaced10a8077e881228c1b
Carol Hsu <sheseee@gmail.com>
2014-11-19 18:13:47 +0800

    script for ruby env install (mac)

--- brewcasek-install.sh
+++
@@ -1,4 +0,0 @@
-brew cask install alfred
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install google-chrome
---
+++ ruby-env-install.sh
@@ -0,0 +1,5 @@
+# ruby environment setup
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install caskroom/cask/brew-cask rbenv ruby-build rbenv-gem-rehash
+brew cask install iterm2 sublime-text
